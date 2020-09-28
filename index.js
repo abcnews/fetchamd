ac7d69df311a683500cbf41defcadd6c4ce6d94f
Ash Kyd <ash@kyd.com.au>
2020-09-29 01:23:55 +1000

    promiseify fetchAmd

--- index.js
+++ index.js
@@ -1,191 +1,158 @@
 /* eslint-disable no-param-reassign */
-/**
- * A private var that captures the last values defined.
- * This allows anonymous defines included accidentally to define without
- * messing up our own calls.
- */
-let lastDefine = false;
+(() => {
+  /**
+   * A private var that captures the last values defined.
+   * This allows anonymous defines included accidentally to define without
+   * messing up our own calls.
+   */
+  let lastDefine = false;
 
-/**
- * Cache for defined objects, so we don't have to reload them again.
- */
-const defined = {};
+  /**
+   * Cache for defined objects, so we don't have to reload them again.
+   */
+  const defined = {};
 
-/**
- * Basic async foreach similar to the `async` library.
- * @param a [Array] The array of objects to iterate over.
- * @param iterator [function] Called with each item in `a`. Two params, value & callback.
- * @param done [function] Callback once array is finished.
- */
-function eachAsync(a, iterator, done) {
-  let count = a.length;
-  const returns = [];
-  function doEach() {
-    count -= 1;
-    iterator(a[count], (returned) => {
-      returns.push(returned);
-      if (count > 0) {
-        doEach(count);
-      } else {
-        returns.reverse();
-        done(returns);
-      }
-    });
-  }
-  doEach();
-}
-
-/**
- * Poll document state until things are good.
- */
-function waitUntil(condition, cb) {
-  const checkInterval = 50; // ms
-  function interval() {
-    if (!condition()) {
-      setTimeout(interval, checkInterval);
-    } else {
-      cb();
-    }
-  }
-  interval();
-}
-
-/**
- * Load a script & call back when it's fired.
- */
-function scriptLoad(module, onload) {
-  const s = document.createElement('script');
-  s.onload = onload;
-  s.src = module;
-  s.async = 'async';
-  document.head.appendChild(s);
-}
-
-/**
- * define
- * Real basic implementation of define. All the magic happens in requireOne.
- * @param name [string] Optional name of the module we're defining.
- * @param deps [array] Optional dependencies
- * @param factory [function] The factory used to create the module.
- */
-function define(name, deps, factory) {
-  // Allow for anonymous modules
-  if (typeof name !== 'string') {
-    // Adjust args appropriately
-    factory = deps;
-    deps = name;
-    name = null;
-  }
-
-  // This module may not have dependencies
-  if (deps instanceof Array === false) {
-    factory = deps;
-    deps = null;
-  }
-
-  lastDefine = {
-    deps,
-    factory,
-  };
-}
-
-// Define function complies with a basic subset of the AMD API
-define.amd = {
-  fetchamd: 1,
-};
-
-function onReady(cb) {
-  if (!window.define) {
-    return cb();
-  }
-
-  // There's already a `define` in the window, and it's not ours.
-  if (!define.amd || !define.amd.fetchamd) {
-    throw new Error('Incompatible mix of defines found in page.');
-  }
-
-  // There's a define from another version of fetchamd, so wait
-  // around until it's finished.
-  return waitUntil(() => typeof window.define === 'undefined', cb);
-}
-
-/**
- * Get one module+dependencies
- * This function gets the specified module, firing off another getMany if
- * the requested module has dependencies.
- * @param module [String] String path to the module to load.
- * @param callback [function] Callback recieves requested module as only argument.
- */
-function requireOne(modulePath, callback) {
-  // Module already loaded? Return it straight away.
-  if (defined[modulePath]) {
-    callback(defined[modulePath]);
-    return;
-  }
-
-  // When the script loads, do the following:
-  function onload() {
-    // Remove it so it has less chance to interfere with anything.
-    window.define = undefined;
-
-    // Make a copy because we may clobber it fetching extra deps.
-    const thisDefine = { ...lastDefine };
-
-    function done(args) {
-      if (thisDefine) {
-        // Apply the factory.
-        // https://github.com/amdjs/amdjs-api/wiki/AMD#factory-
-        if (typeof thisDefine.factory === 'function') {
-          // factory, is a fn that should be executed to
-          // instantiate the module. Tt should only be
-          // executed once.
-          defined[modulePath] = thisDefine.factory.apply(this, (args || []));
-        } else if (typeof thisDefine.factory === 'object') {
-          // If the factory argument is an object, that object
-          // should be assigned as the exported value.
-          defined[modulePath] = thisDefine.factory;
+  /**
+   * Poll document state until things are good.
+   */
+  function waitUntil(condition) {
+    return new Promise((resolve) => {
+      const checkInterval = 50; // ms
+      function interval() {
+        if (!condition()) {
+          setTimeout(interval, checkInterval);
+        } else {
+          resolve();
         }
       }
-
-      // Reply with the last define we defined.
-      callback(defined[modulePath]);
-    }
-
-    // Fetch any extra dependencies if required
-    if (thisDefine.deps && thisDefine.deps.length) {
-      throw new Error(
-        `fetchamd: don't use second level dependencies: ${modulePath}`,
-      );
-    }
-    done();
+      interval();
+    });
   }
 
-  // wait unti the page is ready to start loading.
-  onReady(() => {
-    // Insert a global define method, because that's what AMD scripts look
-    // for. This may interfere with other scripts, so we try to keep it
-    // global for as short a time as possible.
-    window.define = define;
-    scriptLoad(modulePath, onload);
-  });
-}
-
-/**
- * Get many modules + dependencies
- * @param modules [Array] List of modules to load.
- * @param callback [function] Callback recieves each requested module as arguments.
- */
-function getMany(modules, callback) {
-  // Loop through each module & fetch it.
-  eachAsync(modules, (mod, cb) => {
-    requireOne(mod, (requiredMod) => {
-      cb(requiredMod);
+  /**
+   * Load a script & call back when it's fired.
+   */
+  function scriptLoad(moduleSrc) {
+    return new Promise((resolve, reject) => {
+      const s = document.createElement('script');
+      s.onload = resolve;
+      s.onerror = reject;
+      s.src = moduleSrc;
+      s.async = 'async';
+      document.head.appendChild(s);
     });
-  }, (requires) => {
-    // Once we're done, call back with the arguments requested.
-    callback.apply(window, requires);
-  });
-}
+  }
 
-// Expose some globals
-module.exports = getMany;
+  /**
+   * define
+   * Real basic implementation of define. All the magic happens in requireOne.
+   * @param name [string] Optional name of the module we're defining.
+   * @param deps [array] Optional dependencies
+   * @param factory [function] The factory used to create the module.
+   */
+  function define(name, deps, factory) {
+  // Allow for anonymous modules
+    if (typeof name !== 'string') {
+    // Adjust args appropriately
+      factory = deps;
+      deps = name;
+      name = null;
+    }
+
+    // This module may not have dependencies
+    if (deps instanceof Array === false) {
+      factory = deps;
+      deps = null;
+    }
+
+    lastDefine = {
+      deps,
+      factory,
+    };
+  }
+
+  // Define function complies with a basic subset of the AMD API
+  define.amd = {
+    fetchamd: 1,
+  };
+
+  function waitUntilReady() {
+    if (!window.define) {
+      return Promise.resolve();
+    }
+
+    // There's already a `define` in the window, and it's not ours.
+    if (!define.amd || !define.amd.fetchamd) {
+      throw new Error('Incompatible mix of defines found in page.');
+    }
+
+    // There's a define from another version of fetchamd, so wait
+    // around until it's finished.
+    return waitUntil(() => typeof window.define === 'undefined');
+  }
+
+  /**
+   * Get one module+dependencies
+   * This function gets the specified module, firing off another getMany if
+   * the requested module has dependencies.
+   * @param module [String] String path to the module to load.
+   * @param callback [function] Callback recieves requested module as only argument.
+   */
+  function requireOne(modulePath) {
+    // Module already loaded? Return it straight away.
+    if (defined[modulePath]) {
+      return Promise.resolve(defined[modulePath]);
+    }
+
+    return new Promise((resolve, reject) => {
+    // wait unti the page is ready to start loading.
+      waitUntilReady()
+        .then(() => {
+        // Insert a global define method, because that's what AMD scripts look
+        // for. This may interfere with other scripts, so we try to keep it
+        // global for as short a time as possible.
+          window.define = define;
+
+          scriptLoad(modulePath).then(() => {
+            // Remove it so it has less chance to interfere with anything.
+            window.define = undefined;
+
+            // Make a copy because we may clobber it fetching extra deps.
+            const thisDefine = { ...lastDefine };
+
+            // Fetch any extra dependencies if required
+            if (thisDefine.deps && thisDefine.deps.length) {
+              throw new Error(
+                `fetchamd: don't use second level dependencies: ${modulePath}`,
+              );
+            }
+
+            if (thisDefine) {
+              // Apply the factory.
+              // https://github.com/amdjs/amdjs-api/wiki/AMD#factory-
+              if (typeof thisDefine.factory === 'function') {
+                // factory, is a fn that should be executed to
+                // instantiate the module. Tt should only be
+                // executed once.
+                defined[modulePath] = thisDefine.factory.apply(this, []);
+              } else if (typeof thisDefine.factory === 'object') {
+                // If the factory argument is an object, that object
+                // should be assigned as the exported value.
+                defined[modulePath] = thisDefine.factory;
+              }
+            }
+
+            // Reply with the last define we defined.
+            resolve(defined[modulePath]);
+          }).catch(reject);
+        });
+    });
+  }
+
+  // Expose some globals
+  if (typeof module === 'object') {
+    module.exports = requireOne;
+  } else {
+    window.fetchAmd = requireOne;
+  }
+})();
--- test/test.js
+++ test/test.js
@@ -1,110 +1,45 @@
-function getfetchModule(cb){
-    $.ajax({
-        url: '../index.js',
-        dataType: 'text',
-        success: function(script){
-            // Sneaky code to load up a commonjs module
-            // this way we don't need a build task.
-            script = '(function(){var module = {exports:{}};'+script+';return module.exports;})()';
-            var thisExport = eval(script);
-            cb(thisExport);
-        }
-    });
-}
-describe('Require', function(){
-    describe('basic', function(){
-        it('should init', function(done){
-            getfetchModule(function(fetchModule){
-                chai.assert(typeof fetchModule, 'object');
-                done();
-            });
-        });
-        it('can load a single module.', function(done){
-            getfetchModule(function(fetchModule){
-                fetchModule(['requireable1.js'], function(item1){
-                    chai.assert(typeof item1, 'object');
-                    chai.assert(item1.name, 'item one');
-                    done();
-                });
-            });
-        });
-        it('can load multiple modules.', function(done){
-            getfetchModule(function(fetchModule){
-                fetchModule(['requireable1.js', 'requireable2.js'], function(item1, item2){
-                    chai.assert(typeof item1, 'object');
-                    chai.assert(typeof item2, 'object');
-                    chai.assert(item1.name, 'item one');
-                    chai.assert(item2.name, 'item two');
-                    done();
-                });
-            });
-        });
-        it('can have multiple instances.', function(done){
-            getfetchModule(function(fetchModule1){
-                getfetchModule(function(fetchModule2){
-                    chai.assert(fetchModule1 !== fetchModule2, true);
+/* global describe, it, chai, fetchAmd */
 
-                    var i = 0;
-                    fetchModule1(['requireable1.js'], function(item){
-                        chai.assert(typeof item, 'object');
-                        chai.assert(item.name, 'item one');
-                        if(++i === 2){
-                            done();
-                        }
-                    });
-                    fetchModule2(['requireable2.js'], function(item){
-                        chai.assert(typeof item, 'object');
-                        chai.assert(item.name, 'item two');
-                        if(++i === 2){
-                            done();
-                        }
-                    });
-                });
-            });
-        });
-        it('can load a Browserified standalone module.', function(done){
-            getfetchModule(function(fetchModule){
-                fetchModule(['requireable3-browserify.js'], function(item3){
-                    chai.assert(typeof item3, 'number');
-    		// item3 exports the number 5.
-                    chai.assert(item3, 5);
-                    done();
-                });
-            });
-        });
+describe('Require', () => {
+  describe('basic', () => {
+    it('can load a single module.', (done) => {
+      fetchAmd('requireable1.js').then((item1) => {
+        chai.assert(typeof item1, 'object');
+        chai.assert(item1.name, 'item one');
+        done();
+      });
     });
-
-    describe('Popular libraries on cdnjs', function(){
-        it('can load D3', function(done){
-            getfetchModule(function(fetchModule){
-                fetchModule(['http://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js'], function(d3){
-                    chai.assert(typeof d3, 'function');
-    		// item3 exports the number 5.
-                    chai.assert(typeof d3.selectAll, 'function');
-                    done();
-                });
-            });
-        });
-        it('can load Leaflet', function(done){
-            getfetchModule(function(fetchModule){
-                fetchModule(['http://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.js'], function(L){
-                    chai.assert(typeof L, 'object');
-    		// item3 exports the number 5.
-                    chai.assert(typeof L.version, '0.7.3');
-                    done();
-                });
-            });
-        });
-        it('can load Underscore', function(done){
-            getfetchModule(function(fetchModule){
-                fetchModule(['http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js'], function(_){
-                    chai.assert(typeof _, 'function');
-    		// item3 exports the number 5.
-                    chai.assert(typeof _.each, 'function');
-                    done();
-                });
-            });
-        });
+    it('can load a Browserified standalone module.', (done) => {
+      fetchAmd('requireable3-browserify.js').then((item3) => {
+        chai.assert(typeof item3, 'number');
+        // item3 exports the number 5.
+        chai.assert(item3, 5);
+        done();
+      });
     });
+  });
 
+  describe('Popular libraries on cdnjs', () => {
+    it('can load D3', (done) => {
+      fetchAmd('http://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js').then((d3) => {
+        chai.assert(typeof d3, 'function');
+        chai.assert(typeof d3.selectAll, 'function');
+        done();
+      });
+    });
+    it('can load Leaflet', (done) => {
+      fetchAmd('http://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.js').then((L) => {
+        chai.assert(typeof L, 'object');
+        chai.assert(typeof L.version, '0.7.3');
+        done();
+      });
+    });
+    it('can load Underscore', (done) => {
+      fetchAmd('http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js').then((_) => {
+        chai.assert(typeof _, 'function');
+        chai.assert(typeof _.each, 'function');
+        done();
+      });
+    });
+  });
 });
